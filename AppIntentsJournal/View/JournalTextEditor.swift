3efe004f05c513b3e12f4e25c6bd39d86e965d7b
Maria Kharybina <maria.karybina@bbc.co.uk>
2024-11-19 15:55:40 +0000

    added protaction for code while using writing tools

--- AppIntentsJournal/View/JournalTextEditor.swift
+++ AppIntentsJournal/View/JournalTextEditor.swift
@@ -41,6 +41,14 @@ class Coordinator: NSObject, UITextViewDelegate {
     func textViewDidChange(_ textView: UITextView) {
         textEditor.text = textView.attributedText
     }
+    
+    //this let's writing tools know that they shouldn't modify code aka monospaced text
+    //you can enable it for emails, phine numbers or something else - whatever you want to stay unmodified
+    //url protection are already build-in
+    func textView(_ textView: UITextView, writingToolsIgnoredRangesInEnclosingRange enclosingrange: NSRange) -> [NSValue] {
+        guard let journalTextView = textView as? JournalTextView else { return [] }
+        return journalTextView.monospacedRanges(in: enclosingrange) as [NSValue]
+    }
 }
 
 class JournalTextView: UITextView {
